6fc8a720e9eb5a91d4d8118b707f22ec0ebe4877
jrigsbee <jrigsbee@redhat.com>
2017-12-04 14:08:24 -0500

    reverse some edits

--- microservice-session/run.sh
+++ microservice-session/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#/usr/bin/java -jar target/microservice-session-swarm.jar -Dswarm.http.port=5050 -Dswarm.management.http.disable=true $@
-mvn wildfly-swarm:run -Dswarm.http.port=5050 -Dswarm.management.http.disable=true 
+/usr/bin/java -jar target/microservice-session-swarm.jar -Dswarm.http.port=5050 -Dswarm.management.http.disable=true $@
+#mvn wildfly-swarm:run -Dswarm.http.port=5050 -Dswarm.management.http.disable=true 
 #-Dswarm.debug.port=50509
