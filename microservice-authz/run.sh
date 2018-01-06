53bf22a078a37d4597d4178cc730f40a7eb0e591
jrigsbee <jrigsbee@redhat.com>
2018-01-06 08:27:33 -0500

    Made time to live on Json Web Token configurable Use -DtimeToLive=120 for 2 minutes; defaults to 300 seconds (5 mins) Added nonvoter user to leave out Voter role

--- microservice-authz/run.sh
+++ microservice-authz/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/java -jar target/microservice-authz-swarm.jar -Dswarm.http.port=5055 -Dswarm.management.http.disable=true
+/usr/bin/java -jar target/microservice-authz-swarm.jar -Dswarm.http.port=5055 -Dswarm.management.http.disable=true $@
